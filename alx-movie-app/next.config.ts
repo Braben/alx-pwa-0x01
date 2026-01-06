274f5e023ae48da59004eb74e2ba1fcae13f4d5c
Benjamin Lamptey <kbenlamptey@gmail.com>
2026-01-06 15:58:17 +0000

    Configure PWA in Next.js

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,28 +1,49 @@
 // import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// //   reactCompiler: true,
+// //   reactStrictMode: true,
+// // };
+
+// // export default nextConfig;
+
+// import withPWAInit from "@ducanh2912/next-pwa";
+
+// /** @type {import('next').NextConfig} */
+
+// const withPWA = withPWAInit({
+//   dest: "public",
+// });
+
+// const nextConfig = {
 //   reactCompiler: true,
 //   reactStrictMode: true,
+//   images: {
+//     domains: ["m.media-amazon.com"],
+//   },
 // };
 
-// export default nextConfig;
+// export default withPWA({
+//   ...nextConfig,
+// });
 
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-
 const withPWA = withPWAInit({
   dest: "public",
 });
 
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: ["m.media-amazon.com"],
   },
+
+  // ✅ Explicitly disable Turbopack
+  turbopack: {},
 };
 
-export default withPWA({
-  ...nextConfig,
-});
+export default withPWA(nextConfig);
